630ac2aac3ffcde294db7bff30a2e248f9b5cec4
sanyament <billion1cash1337@gmail.com>
2025-10-09 00:47:48 +0300

    Добавлена директория с запросами
    - В директории querys_sql находятся запросы к базе данных
    - База данных с помощью mysql with xampp
    - Добавлен файл querys_sql/createTables.sql в котором хранятся уже
      запросы с пометками --was runned и --will runned

---
+++ querys_sql/createTables.sql
@@ -0,0 +1,39 @@
+CREATE TABLE Role(
+    Id int AUTO_INCREMENT PRIMARY KEY,
+    Name varchar(50) UNIQUE);
+-- was runned
+CREATE TABLE User(
+	Id int AUTO_INCREMENT PRIMARY KEY,
+    Username varchar(100) UNIQUE,
+    Fname varchar(50),
+    Lname varchar(50),
+    IdRole int,
+    FOREIGN KEY (IdRole) REFERENCES Role(Id)
+);
+-- was runned
+CREATE TABLE TaskStatus(
+    Id int AUTO_INCREMENT PRIMARY KEY,
+    Name varchar(50) UNIQUE);
+CREATE TABLE TaskCategory(
+    Id int AUTO_INCREMENT PRIMARY KEY,
+    Name varchar(50) UNIQUE);
+CREATE TABLE TaskImportance(
+    Id int AUTO_INCREMENT PRIMARY KEY,
+    Name varchar(50) UNIQUE,
+    Grade int UNIQUE);
+-- was runned
+
+create table Tasks(
+    Id int AUTO_INCREMENT PRIMARY KEY,
+    IdOwner int,
+    IdStatus int,
+    IdCategory int,
+    IdImportance int,
+    Title varchar(100),
+    Description text,
+	FOREIGN KEY (IdOwner) REFERENCES user(Id),
+    FOREIGN KEY (IdStatus) REFERENCES taskstatus(Id),
+    FOREIGN KEY (IdCategory) REFERENCES taskcategory(Id),
+    FOREIGN KEY (IdImportance) REFERENCES taskimportance(Id)
+);
+--was runned
