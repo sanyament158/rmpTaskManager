4b56959376a769052567525d3c193382b76fb1f0
sanyament <billion1cash1337@gmail.com>
2025-10-15 06:13:22 +0700

    Написана логика клиента (авторизация)
    - Все запустилось, но я не тестировал
    - Только авторизация, юзеров добавлять через postman
    - В итоге добавлен метод, который возвращает обьект UserResponse

--- android/app/src/main/java/DataClasses/UserResponse.kt
+++ android/app/src/main/java/DataClasses/UserResponse.kt
@@ -1,9 +1,9 @@
 package DataClasses
 
 data class UserResponse(
-    val Id: Int,
+    val Id: Int? = null,
     val Username: String,
-    val Fname: String,
-    val Lname: String,
+    val Fname: String? = null,
+    val Lname: String? = null,
     val IdRole: Int
 )
--- android/app/src/main/java/com/example/taskmanager/LoginActivity.kt
+++ android/app/src/main/java/com/example/taskmanager/LoginActivity.kt
@@ -1,11 +1,21 @@
 package com.example.taskmanager
 
+import DataClasses.UserRequest
+import DataClasses.UserResponse
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.ui.text.LinkAnnotation
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.taskmanager.databinding.ActivityLoginBinding
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.OutputStream
+import java.net.HttpURLConnection
+import java.net.URL
 
 class LoginActivity : AppCompatActivity() {
     lateinit var binding: ActivityLoginBinding
@@ -16,7 +26,62 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         with(binding){
-            
+            btnSubmit.setOnClickListener {
+                if (etUsername.text != null && etPassword.text != null){
+                    val username: String = etUsername.text.toString()
+                    val password: String = etPassword.text.toString()
+                    val userRequestObj: UserRequest = UserRequest(username, password)
+
+
+
+                }
+            }
         }
+    }
+    private fun AuthorizationUser(username: String, password: Int): UserResponse{
+        // work with api
+        lateinit var responseJson: JSONObject
+        lateinit var responseUser: UserResponse
+        try {
+            val url: URL = URL("http://192.168.0.151/taskmanagerapi/login/login.php")
+            val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+            connection.requestMethod = "POST"
+            connection.doInput = true
+            connection.setRequestProperty("Content-Type", "application/json")
+
+            val json: JSONObject = JSONObject()
+            json.put("username", username)
+            json.put("password", password)
+
+            val os: OutputStream = connection.outputStream
+            os.write(json.toString().toByteArray(Charsets.UTF_8))
+            os.flush()
+            os.close()
+
+            val responseCode: Int = connection.responseCode
+
+            if (responseCode == HttpURLConnection.HTTP_OK){
+                val responseText: String = connection.inputStream.use { inputStream ->
+                    inputStream.reader(Charsets.UTF_8).use { reader ->
+                        reader.readText()
+                    }
+                }
+                responseJson = JSONObject(responseText)
+            }
+        }
+        catch (e: Exception){
+            Log.e("work with API ERROR (AUTHORIZATION)", e.message.toString())
+        }
+
+        // fetch from response json
+        try{
+            if (responseJson.getBoolean("success")){
+                responseUser =  UserResponse(
+                    Username = responseJson.getString("username"),
+                    IdRole = responseJson.getInt("idrole")
+                )
+            }
+        } catch (e: Exception) { Log.e("fetch from json ERROR (AUTHORIZATION)", e.message.toString()) }
+        return responseUser
     }
 }
